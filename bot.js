de3c7c70fb2e68872ef6723b0d18794f7b23d165
Nitin Ranganath <nitinranganath@gmail.com>
2021-03-05 15:50:24 +0530

    Implement ping command

--- bot.js
+++ bot.js
@@ -10,3 +10,19 @@ const bot = new Client();
 
 // Log our bot in
 bot.login(process.env.DISCORD_BOT_TOKEN);
+
+// Log to console when bot is ready
+bot.on('ready', () => {
+  console.log(`${bot.user.username} is up and running!`);
+});
+
+// Reply to user messages
+bot.on('message', async (message) => {
+  // Do not reply if message was sent by bot
+  if (message.author.bot) return;
+
+  // Reply to !ping
+  if (message.content.startsWith('!ping')) {
+    return message.reply('I am working!');
+  }
+});
