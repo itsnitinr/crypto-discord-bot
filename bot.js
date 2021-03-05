2a1ccf288449653668ffb284ecfc9a08ad5f84f5
Nitin Ranganath <nitinranganath@gmail.com>
2021-03-05 19:09:29 +0530

    Implement the news command

--- bot.js
+++ bot.js
@@ -58,4 +58,31 @@ bot.on('message', async (message) => {
       }
     }
   }
+
+  // Reply to !news
+  if (message.content.startsWith('!news')) {
+    try {
+      const { data } = await axios.get(
+        `https://newsapi.org/v2/everything?q=crypto&apiKey=${process.env.NEWS_API_KEY}&pageSize=1&sortBy=publishedAt`
+      );
+
+      // Destructure useful data from response
+      const {
+        title,
+        source: { name },
+        description,
+        url,
+      } = data.articles[0];
+
+      return message.reply(
+        `Latest news related to crypto:\n
+        Title: ${title}\n
+        Description:${description}\n
+        Source: ${name}\n
+        Link to full article: ${url}`
+      );
+    } catch (err) {
+      return message.reply('There was an error. Please try again later.');
+    }
+  }
 });
